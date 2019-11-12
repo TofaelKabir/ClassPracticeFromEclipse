0aaddc12c805ca4e2d50d93ef6fc8da3b6268062
MTKS <45343271+tofaelKabir@users.noreply.github.com>
2019-11-12 15:07:57 -0500

    Update CompareNumer3.java

--- src/logicalStatement/CompareNumer3.java
+++ src/logicalStatement/CompareNumer3.java
@@ -4,14 +4,12 @@ public class CompareNumer3 {
 
 	public static void main(String[] args) {
 		int value1 = 63;
-		int value2 = 53;
-		int value3 = 45;
-		if (value1>value2 & value3>50) {
+		int value2 = 63;
+		if (value1>value2) {
 			System.out.println(value1+"  is bigger than  "+value2);
 		}else if (value1<value2){
 			System.out.println(value1+ "  is smaller than  "+value2);
-		//}else if(value1==value2){ //else if used, when we use the logic (Value1==Value2) OR WE CAN USE ONLY else
-		}else { 
+		}else{ 
 			System.out.println(value1+"  is equal to  "+value2);
 		}
 
